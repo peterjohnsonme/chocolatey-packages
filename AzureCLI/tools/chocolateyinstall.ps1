a1b155764f8e95474d09dbe0205aefc2a353079a
peterjohnsonme <peter@peterjohnson.me>
2017-07-13 12:06:09 -0500

    Initial checkin.  Added Azure CLI for Windows.

---
+++ AzureCLI/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'azure-cli-windows'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://azurecliprod.blob.core.windows.net/msi/azure-cli-2.0.10.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  softwareName  = 'Azure CLI for Windows*'
+  checksum      = 'f533f0e78c33cb22b96c058f4056a82140885389'
+  checksumType  = 'sha1'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
