7c33dd5d10cf11ddc0f0bc67262e07b0372ed3ec
peterjohnsonme <peter@peterjohnson.me>
2017-07-13 12:39:07 -0500

    Fixed some package errors.

--- AzureCLI/tools/chocolateyinstall.ps1
+++ AzureCLI/tools/chocolateyinstall.ps1
@@ -6,15 +6,15 @@ $url        = 'https://azurecliprod.blob.core.windows.net/msi/azure-cli-2.0.10.m
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
-  url           = $url
+  url64bit      = $url
+  fileType      = "msi"
 
   softwareName  = 'Azure CLI for Windows*'
-  checksum      = 'f533f0e78c33cb22b96c058f4056a82140885389'
-  checksumType  = 'sha1'
+  checksum64      = 'f533f0e78c33cb22b96c058f4056a82140885389'
+  checksumType64  = 'sha1'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
+Install-ChocolateyPackage @packageArgs
