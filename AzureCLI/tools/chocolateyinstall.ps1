338c503eb5fd9d4d2cf93b7765e679fcaec62d01
Peter Johnson <peter.johnson@incontextsolutions.com>
2018-01-17 14:49:12 -0600

    Updated azure-cli to 2.0.25

--- AzureCLI/tools/chocolateyinstall.ps1
+++ AzureCLI/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'azure-cli'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://azurecliprod.blob.core.windows.net/msi/azure-cli-2.0.24.msi'
+$url        = 'https://azurecliprod.blob.core.windows.net/msi/azure-cli-2.0.25.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = "msi"
 
   softwareName  = 'Microsoft CLI 2.0 for Azure'
-  checksum      = 'e44ab531694317991bb516feef625ed059b0f34b'
+  checksum      = '0ed15bb587aeb219da04119cc039ece76c5e6e64'
   checksumType  = 'sha1'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
