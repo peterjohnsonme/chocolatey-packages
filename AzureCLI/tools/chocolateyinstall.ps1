64f4e3fb33255e62babca789220ffe57cf772702
Peter Johnson <peter.johnson@incontextsolutions.com>
2018-03-16 12:04:07 -0500

    Updated azure-cli to 2.0.20

--- AzureCLI/tools/chocolateyinstall.ps1
+++ AzureCLI/tools/chocolateyinstall.ps1
@@ -1,20 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'azure-cli'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file       = Join-Path $toolsDir 'azure-cli-2.0.28.msi'
+$url        = 'https://azurecliprod.azureedge.net/msi/azure-cli-2.0.29.msi'
 
 $packageArgs = @{
   packageName   = $packageName
-  file      		= $file
+  url      		  = $url
   fileType      = "msi"
 
   softwareName  = 'Microsoft CLI 2.0 for Azure'
-  checksum      = '1eba80789ec66cb0731961f8ec8eb17a1884a9e3'
+  checksum      = 'ba9c513aec820bbe2b0f2f086628478657a1f1ee'
   checksumType  = 'sha1'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
