fa7e9d9d5e69b72ca04a00953e31494e24296516
Peter Johnson <peter@peterjohnson.me>
2019-01-12 13:48:10 -0600

    Update to 2.0.54

--- AzureCLI/tools/chocolateyinstall.ps1
+++ AzureCLI/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'azure-cli'
-$url        = 'https://azurecliprod.azureedge.net/msi/azure-cli-2.0.52.msi'
+$url        = 'https://azurecliprod.azureedge.net/msi/azure-cli-2.0.54.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = "msi"
 
   softwareName  = 'Microsoft CLI 2.0 for Azure'
-  checksum      = 'e3dde746a6a3858d9b36cb7b96f04eef28e65957'
+  checksum      = '2c413f55e2db2d1b927106143896f8f66ef46dd4'
   checksumType  = 'sha1'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
