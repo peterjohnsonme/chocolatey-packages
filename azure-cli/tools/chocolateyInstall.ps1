2d11304a4a5940e76b46d5416b80875fdb8d1d25
peterjohnsonme <1328982+peterjohnsonme@users.noreply.github.com>
2019-03-13 18:08:09 +0000

    AU: 1 updated - azure-cli [skip ci]

--- azure-cli/tools/chocolateyInstall.ps1
+++ azure-cli/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
     fileType       = "msi"
  
     softwareName   = 'Microsoft CLI 2.0 for Azure'
-    checksum       = ''
-    checksumType   = ''
+    checksum       = '57a2fc38f44c1ec37ae66cbc77f72e430396b6d95d87b0a8f48c846c1525d782'
+    checksumType   = 'sha256'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
