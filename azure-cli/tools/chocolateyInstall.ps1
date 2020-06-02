3990198842f385c679691ed39ffa2e4e9ee49ba0
peterjohnsonme <1328982+peterjohnsonme@users.noreply.github.com>
2020-06-02 08:01:05 +0000

    AU: 1 updated - azure-cli [skip ci] https://gist.github.com/8f131672f10ddadd06eac53ae5fff3ce/0c87de456ba43a580f45559c5c398bda2382c2e9

--- azure-cli/tools/chocolateyInstall.ps1
+++ azure-cli/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $packageName = 'azure-cli'
  
 $packageArgs = @{
     packageName    = $packageName
-    url            = 'https://azurecliprod.blob.core.windows.net/msi/azure-cli-2.6.0.msi'
+    url            = 'https://azurecliprod.blob.core.windows.net/msi/azure-cli-2.7.0.msi'
     fileType       = "msi"
  
     softwareName   = 'Microsoft CLI 2.0 for Azure'
-    checksum       = 'b7fb439ab63dbea939d8f1d4ba2530dbb5939673e9450ce8ef5b55a71c22d68b'
+    checksum       = 'c2460b16d02526c43ff10b614fd5c206f9c2577a9d4564cc18ec718dc5af8e38'
     checksumType   = 'sha256'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
