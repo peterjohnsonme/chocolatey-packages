195dbe95dd0c60b5519abc32a8f0379ba2f7d8cf
Peter Johnson <peter@peterjohnson.me>
2020-07-13 15:15:52 -0500

    Update update.ps1

--- azure-documentdb-data-migration-tool/update.ps1
+++ azure-documentdb-data-migration-tool/update.ps1
@@ -4,7 +4,7 @@ $releases = 'https://aka.ms/csdmtool'
 
 function global:au_SearchReplace {
     @{
-        ".\tools\chocolateyInstall.ps1" = @{
+        ".\tools\chocolateyinstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
