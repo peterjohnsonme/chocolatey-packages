18e2a6f3568cd7f1ddedb66b4149cfcf6c90d16c
Mark Wragg <mark@wragg.io>
2021-12-08 18:53:00 +0000

    Update chocolateyinstall.ps1
    Fixed path to the tool as the previous one no longer works. I updated the checksum by using Get-FileHash with the Algorithm set to SHA1, but I've not done this sort of update before so I don't know if that was necessarily correct.

--- azure-documentdb-data-migration-tool/tools/chocolateyinstall.ps1
+++ azure-documentdb-data-migration-tool/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName= 'azure-documentdb-data-migration-tool'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cosmosdbtools.blob.core.windows.net/datamigrationtool/2019.09.23-1.8.3/dt1.8.3.zip'
+$url        = 'https://github.com/Azure/azure-documentdb-datamigrationtool/releases/download/1.8.3/azure-documentdb-datamigrationtool-1.8.3.zip'
 
-$checksum     = 'f594b2478b0904a7aa8022db872d0d66480c4fb1'
+$checksum     = '329694305994F95FB4A877F7D769D93F4900A3E6'
 $checksumType = 'sha1'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
