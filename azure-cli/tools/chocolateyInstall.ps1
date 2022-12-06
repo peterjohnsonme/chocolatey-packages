e5ddd78419edc637fdce3916b704660871ae3bc1
peterjohnsonme <1328982+peterjohnsonme@users.noreply.github.com>
2022-12-06 08:02:18 +0000

    AU: 1 updated - azure-cli [skip ci] https://gist.github.com/8f131672f10ddadd06eac53ae5fff3ce/0fa5813386191e47b7ce442f9a49b9b08e66e4fc

--- azure-cli/tools/chocolateyInstall.ps1
+++ azure-cli/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $packageName = 'azure-cli'
  
 $packageArgs = @{
     packageName    = $packageName
-    url            = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.42.0.msi'
+    url            = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.43.0.msi'
     fileType       = "msi"
  
     softwareName   = 'Microsoft CLI 2.0 for Azure'
-    checksum       = '70e18e9f782c29e482a10661f97a1449dea7d681ac559aad8f3f33e32961e74c'
+    checksum       = 'a8a054b29095ca528ad76e068781fb8ebbfd8bd71ab231e1b864aee94ea205dc'
     checksumType   = 'sha256'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
