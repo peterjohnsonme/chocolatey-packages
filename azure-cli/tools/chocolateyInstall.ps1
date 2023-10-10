2e3440ef624f1e783202b20c446970e394777a51
Peter Johnson <peter@peterjohnson.me>
2023-10-10 08:49:51 -0500

    Add 64-bit support to chocolateyInstall

--- azure-cli/tools/chocolateyInstall.ps1
+++ azure-cli/tools/chocolateyInstall.ps1
@@ -5,11 +5,14 @@ $packageName = 'azure-cli'
 $packageArgs = @{
     packageName    = $packageName
     url            = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.53.0.msi'
+    url64          = 'changeme'
     fileType       = "msi"
  
     softwareName   = 'Microsoft CLI 2.0 for Azure'
     checksum       = '7b7a3815e0a905288b461da2da063fbd415325e77066fdfeb8c5525b9f87412d'
     checksumType   = 'sha256'
+    checksum64     = 'changeme'
+    checksum64Type = 'changeme'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
