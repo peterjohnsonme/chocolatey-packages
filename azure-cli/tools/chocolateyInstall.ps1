e7d5f2365ae0c155d8456cec48b0a98339158764
peterjohnsonme <1328982+peterjohnsonme@users.noreply.github.com>
2024-01-10 08:01:30 +0000

    AU: 1 updated - azure-cli [skip ci] https://gist.github.com/peterjohnsonme/8f131672f10ddadd06eac53ae5fff3ce/da7141773cba6c9f8cc3715c42192b611217d35c

--- azure-cli/tools/chocolateyInstall.ps1
+++ azure-cli/tools/chocolateyInstall.ps1
@@ -4,14 +4,14 @@ $packageName = 'azure-cli'
  
 $packageArgs = @{
     packageName    = $packageName
-    url            = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.55.0.msi'
-    url64          = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.55.0-x64.msi'
+    url            = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.56.0.msi'
+    url64          = 'https://azcliprod.blob.core.windows.net/msi/azure-cli-2.56.0-x64.msi'
     fileType       = "msi"
  
     softwareName   = 'Microsoft CLI 2.0 for Azure'
-    checksum       = 'f80b1e18fff8ce5c710fee9bebd0d720ccb0713a22aa9976040b3ee237cd7406'
+    checksum       = '61e31dacfd08bea2b3a00b11bcef91020df9cee447319b813da951a7bccfaf3b'
     checksumType   = 'sha256'
-    checksum64     = 'a63d4ae1f9da4b9973247ebe80e1a1e20f915b8d0dc95082cc6a6ed22043208c'
+    checksum64     = 'a46d325b740c73cb1f5f05791bdb89393000189b5fbf41a5fafa8b4834a58e1b'
     checksum64Type = 'sha256'
  
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
